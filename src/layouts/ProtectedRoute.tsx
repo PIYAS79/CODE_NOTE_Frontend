8440980aacfb8062b7095e55afa09ffd0a2a3d6a
S M Piyas Mahamude Alif <piyasmahmudealif@gmail.com>
2024-07-05 02:34:09 +0600

    crate Proteced Route

---
+++ src/layouts/ProtectedRoute.tsx
@@ -0,0 +1,22 @@
+import {ReactNode} from 'react';
+import { useAppSelector } from '../redux/hooks';
+import { Navigate } from 'react-router-dom';
+
+
+/**
+ * protected route ke ami Central Layout a lagaite pari , if(amr full project protected rakhte chay | except login and reg, i know why !)
+ * ar jodi only kichu kichu page protected rakhte hoi tahole route.tsx a giye akta akta element a wrap kore aste hobe ! 
+ */
+
+
+const ProtectedRoute = ({children}:{children:ReactNode}) => {
+  
+    const token = useAppSelector(state=>state.auth.token);
+    if(!token){
+        return <Navigate to={'/login'} replace={true}/>
+    }
+
+    return children
+}
+
+export default ProtectedRoute
--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -1,11 +1,12 @@
 import MainLayouts from "../layouts/MainLayouts"
+import ProtectedRoute from "../layouts/ProtectedRoute"
 
 
 const RootPage = () => {
     return (
-        <>
+        <ProtectedRoute>
             <MainLayouts />
-        </>
+        </ProtectedRoute>
     )
 }
 
